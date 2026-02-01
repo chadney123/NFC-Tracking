33de32257ff89b6cd09d18449613d0cc18d5e880
chadney123 <chadney123@yahoo.com>
2026-02-01 23:10:39 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
-self.addEventListener('fetch', function(event) {
-    // Allows the PWA to be installable
+self.addEventListener('install', (event) => {
+    self.skipWaiting(); // Forces the waiting service worker to become the active one
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim()); // Immediately take control of the page
+});
+
+self.addEventListener('fetch', (event) => {
+    // Allows the app to function and be installable
 });
